f50e5df81f221fd35f8a7e8d52babeb647d98050
Joshua Geisler <geislerjosh@gmail.com>
2016-04-23 23:16:28 -0400

    on to 8 mofos

---
+++ problem_7.py
@@ -0,0 +1,17 @@
+def isPrime(n):
+	for x in range(2, n/2):
+		if n % x == 0:
+			return False
+	return True
+
+def find_specific_prime(thisPrime):
+	prime_counter = 0
+	x = 1
+	while True:
+		x += 1
+		if isPrime(x):
+			prime_counter += 1
+		if prime_counter > thisPrime:
+			return x
+
+print(find_specific_prime(10001))
